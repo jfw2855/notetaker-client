68240189f999abbe2cc373ec3266a4a5ecfa1c3d
jfw2855 <jwaddle1130@gmail.com>
2022-05-13 09:58:21 -0500

    adds intial components to test if client is working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import { Header } from './components/Header';
+import { NotesList } from './components/pages/NotesList';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>connected to local host 3000 😍</h1>
+      <Header/>
+      <NotesList/>
     </div>
   );
 }
